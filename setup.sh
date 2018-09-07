42eb05e5c5d2ee65158449ca913430c9a190d40f
Ilya 'Ilirium' Kalimulin <ilirium115@gmail.com>
2018-09-07 16:16:39 +0700

    updated Anaconda, CUDA and cuDNN to the last

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+# *** ***
+# Settings and Packages
 red=`tput setaf 1`
 green=`tput setaf 2`
 blue=`tput setaf 4`
@@ -19,6 +21,8 @@ sudo apt install unzip git -y
 echo "${red}Removing unneeded packages...${reset}"
 sudo apt -y autoremove
 
+# *** ***
+# Anaconda
 echo "${bg}                         ${reset}"
 echo "${bg}${black} Downloading ${green}Anaconda${black}... ${reset}"
 echo "${bg}                         ${reset}"
@@ -26,14 +30,17 @@ cd ~
 sudo rm -R downloads
 mkdir downloads
 cd downloads
-wget https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
+wget https://repo.continuum.io/archive/Anaconda3-5.2.0-Linux-x86_64.sh
 
 echo "${green}Done! Installing Anaconda...${reset}"
-bash Anaconda3-5.0.1-Linux-x86_64.sh -b
+bash Anaconda3-5.2.0-Linux-x86_64.sh -b
 echo "${bg}                                       ${reset}"
 echo "${bg}${black} Finishing running ${green}Anaconda${black} installer. ${reset}"
 echo "${bg}                                       ${reset}"
 echo ""
+
+# *** ***
+# Fastai
 echo "${bg}                           ${reset}"
 echo "${bg}${black} Downloading ${blue}fast.ai${black} v2... ${reset}"
 echo "${bg}                           ${reset}"
@@ -76,26 +83,32 @@ echo "c.NotebookApp.open_browser = False" >> ~/.jupyter/jupyter_notebook_config.
 echo "Enabling access through firewall (ufw) ..."
 sudo ufw allow 8888/tcp
 
+# *** ***
+# CUDA and cuDNN
 echo "${bg}                                          ${reset}"
-echo "${bg}${black} Installing ${green}NVIDIA${black} drivers + ${green}CUDA${black} 9.0 ... ${reset}"
+echo "${bg}${black} Installing ${green}NVIDIA${black} drivers + ${green}CUDA${black} 9.2 ... ${reset}"
 echo "${bg}                                          ${reset}"
 sudo apt -y install qtdeclarative5-dev qml-module-qtquick-controls
 echo "Adding ${green}NVIDIA repository...${reset}"
 sudo add-apt-repository ppa:graphics-drivers/ppa -y
 sudo apt update
 cd ~/downloads/
-echo "Installing ${green}CUDA${reset} 9.0"
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+echo "Installing ${green}CUDA${reset} 9.2"
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.2.148-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1604_9.2.148-1_amd64.deb
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 sudo apt update
 sudo apt install cuda -y
-echo "Installing ${green}cuDNN${reset} 9.1"
-wget http://files.fast.ai/files/cudnn-9.1-linux-x64-v7.tgz
-tar xf cudnn-9.1-linux-x64-v7.tgz
-sudo cp cuda/include/*.* /usr/local/cuda/include/
-sudo cp cuda/lib64/*.* /usr/local/cuda/lib64/
+echo "Installing ${green}cuDNN${reset} 7.2.1.38"
+wget http://ftp.heanet.ie/mirrors/ftp.archlinux.org/community/os/x86_64/cudnn-7.2.1-1-x86_64.pkg.tar.xz
+mkdir cudnn
+tar xf cudnn-7.2.1-1-x86_64.pkg.tar.xz -C cudnn
+sudo cp cudnn/opt/cuda/include/cudnn.h /usr/local/cuda/include
+sudo cp cudnn/opt/cuda/lib64/libcudnn* /usr/local/cuda/lib64
+sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
 
+# *** ***
+# iPyWidgets
 echo "Installing iPyWidgets for Jupyter Notebook"
 pip install ipywidgets
 jupyter nbextension enable --py widgetsnbextension --sys-prefix
